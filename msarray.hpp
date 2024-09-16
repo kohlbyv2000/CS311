b4cecbc8dfdd96c5a3f78d1e9b8fd6931d1c3077
kohlbyv2000 <156946729+kohlbyv2000@users.noreply.github.com>
2024-09-16 00:19:53 +0000

    Completed big five functions

--- msarray.hpp
+++ msarray.hpp
@@ -8,6 +8,7 @@
 
 #include <cstddef>
 #include <iterator>
+#include <algorithm>
 
 
 ////**** MSARRAY CLASS TEMPLATE ****////
@@ -19,15 +20,16 @@ class MSArray {
 
     public:
 
+    // 
     using size_type = std::size_t;
     using value_type = ValType;
+
 
     //** Ctor Functions and Big Five **//
 
     // Default ctor
-    explicit MSArray(size_type size) {
-        :_arrayptr(new value_type 8);
-    }
+    explicit MSArray(size_type size)
+        :_arrayptr(new value_type[8]) {}
 
     // Dctor
     ~MSArray() {
@@ -35,24 +37,44 @@ class MSArray {
     }
 
     // Copy ctor
-    MSArray(const MSArray &other) {
-        
-    }
+    MSArray(const MSArray &other)
+        :_arrayptr(new value_type[8]) {
+            std::copy(other._arrayptr.begin(), 
+            other._arrayptr.end(), 
+            other._arrayptr.begin());
+        }
 
     // Copy assignment operator
     MSArray &operator=(const MSArray &rhs) {
-        
+        auto rhs_copy = rhs;
+        arrayswap(rhs_copy);
+        return *this;
     }
 
     // Move ctor
-    MSArray(MSArray && other) {
-        
-    }
+    MSArray(MSArray && other) noexcept 
+        :_arrayptr(std::move(other._arrayptr)) {
+            delete [] other._arrayptr;
+        }
 
     // Move assignment operator
-    MSArray &operator=(MSArray &rhs) {
-
+    MSArray &operator=(MSArray &rhs) noexcept {
+        arrayswap(rhs);
+        return *this;
     }
+
+
+    //** Operators **/
+    
+
+    //** Private data members/functions **/
+    private:
+
+    void arrayswap(MSArray &other) noexcept {
+        std::swap(_arrayptr, other._arrayptr);
+    }
+
+    value_type *_arrayptr;
     
 }; // End of MSArray class template
 
