924b8d07667dfd8e0cb5818dc6181ea98bb5e4e4
kohlbyv2000 <156946729+kohlbyv2000@users.noreply.github.com>
2024-10-28 02:57:27 +0000

    Started

---
+++ gfsarray.hpp
@@ -0,0 +1,195 @@
+// gfsarray.hpp
+// Adam Bishop and Kohlby Vierthaler
+// 2024/10/27
+// Header file for frightfully smart array
+
+
+#ifndef FILE_GFSARRAY_HPP_INCLUDED
+#define FILE_GFSARRAY_HPP_INCLUDED
+
+#include <cstddef>
+// For std::size_t
+#include <algorithm>
+// For std::max
+
+
+// *********************************************************************
+// class GFSArray - Class definition
+// *********************************************************************
+
+
+// class GFSArray
+// Generic Frightfully Smart Array
+// Resizable, copyable/movable, exception-safe.
+// Invariants:
+//     0 <= _size <= _capacity.
+//     _data points to an array of value_type, allocated with new [],
+//      owned by *this, holding _capacity value_type values -- UNLESS
+//      _capacity == 0, in which case _data may be nullptr.
+class GFSArray {
+
+// ***** GFSArray: types *****
+public:
+
+    // value_type: type of data items
+    using value_type = int;
+
+    // size_type: type of sizes & indices
+    using size_type = std::size_t;
+
+    // iterator, const_iterator: random-access iterator types
+    using iterator = value_type *;
+    using const_iterator = const value_type *;
+
+// ***** GFSArray: internal-use constants *****
+private:
+
+    // Capacity of default-constructed object
+    enum { DEFAULT_CAP = 42 };
+
+// ***** GFSArray: ctors, op=, dctor *****
+public:
+
+    // Default ctor & ctor from size
+    // Strong Guarantee
+    explicit GFSArray(size_type thesize=0)
+        :_capacity(std::max(thesize, size_type(DEFAULT_CAP))),
+            // _capacity must be declared before _data
+         _size(thesize),
+         _data(_capacity == 0 ? nullptr
+                              : new value_type[_capacity])
+    {}
+
+    // Copy ctor
+    // Strong Guarantee
+    GFSArray(const GFSArray & other);
+
+    // Move ctor
+    // No-Throw Guarantee
+    GFSArray(GFSArray && other) noexcept;
+
+    // Copy assignment operator
+    // ??? Guarantee
+    GFSArray & operator=(const GFSArray & other);
+
+    // Move assignment operator
+    // No-Throw Guarantee
+    GFSArray & operator=(GFSArray && other) noexcept;
+
+    // Dctor
+    // No-Throw Guarantee
+    ~GFSArray()
+    {
+        delete [] _data;
+    }
+
+// ***** GFSArray: general public operators *****
+public:
+
+    // operator[] - non-const & const
+    // Pre:
+    //     ???
+    // No-Throw Guarantee
+    value_type & operator[](size_type index)
+    {
+        return _data[index];
+    }
+    const value_type & operator[](size_type index) const
+    {
+        return _data[index];
+    }
+
+// ***** GFSArray: general public functions *****
+public:
+
+    // size
+    // No-Throw Guarantee
+    size_type size() const noexcept
+    {
+        return _size;
+    }
+
+    // empty
+    // No-Throw Guarantee
+    bool empty() const noexcept
+    {
+        return size() == 0;
+    }
+
+    // begin - non-const & const
+    // No-Throw Guarantee
+    iterator begin() noexcept
+    {
+        return _data;
+    }
+    const_iterator begin() const noexcept
+    {
+        return _data;
+    }
+
+    // end - non-const & const
+    // No-Throw Guarantee
+    iterator end() noexcept
+    {
+        return begin() + size();
+    }
+    const_iterator end() const noexcept
+    {
+        return begin() + size();
+    }
+
+    // resize
+    // ??? Guarantee
+    void resize(size_type newsize);
+
+    // insert
+    // Pre:
+    //     ???
+    // ??? Guarantee
+    iterator insert(iterator pos,
+                    value_type item);
+        // Above, passing by value is appropriate, since our value type
+        // is int. However, if the value type is changed, then a
+        // different parameter-passing method may need to be used.
+
+    // erase
+    // Pre:
+    //     ???
+    // ??? Guarantee
+    iterator erase(iterator pos);
+
+    // push_back
+    // ??? Guarantee
+    void push_back(value_type item)
+        // Above, passing by value is appropriate, since our value type
+        // is int. However, if the value type is changed, then a
+        // different parameter-passing method may need to be used.
+    {
+        insert(end(), item);
+    }
+
+    // pop_back
+    // Pre:
+    //     ???
+    // ??? Guarantee
+    void pop_back()
+    {
+        erase(end()-1);
+    }
+
+    // swap
+    // No-Throw Guarantee
+    void swap(GFSArray & other) noexcept;
+
+// ***** GFSArray: data members *****
+private:
+
+    // Below, _capacity must be declared before _data
+    size_type    _capacity;  // Size of our allocated array
+    size_type    _size;      // Size of client's data
+    value_type * _data;      // Pointer to our array
+
+};  // End class GFSArray
+
+
+#endif  //#ifndef FILE_GFSARRAY_HPP_INCLUDED
